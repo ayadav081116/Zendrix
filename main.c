f48fd7129fc0021c84a135082539e00c156ed7a0
Ashish yadav <Ashish.yadav@socgen.com>
2020-08-22 08:00:52 +0000

    updated main.c file

---
+++ main.c
@@ -0,0 +1,28 @@
+eudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+
+
+Initial Security()
+{
+ //Sample Code
+}
+
+feature1()
+{
+  //Sample Code for Feature 1
+}
+
+
+feaature2()
+{
+  //Sample Code for Feature 2
+}
+
+
+
+}
